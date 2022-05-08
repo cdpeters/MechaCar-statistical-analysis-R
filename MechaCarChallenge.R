94980fd488b5544e50a725fbe95e01b11aa53111
Chris Peterson <cdpeters1@gmail.com>
2022-05-07 17:22:17 -0700

    Add final rscript and README

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -26,8 +26,8 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 # Read in Data -----------------------------------------------------------------
 # Read in MechaCar_mpg csv file
 Suspension_Coil <- read.csv(file='Suspension_Coil.csv',
-                         check.names=F,
-                         stringsAsFactors=F)
+                            check.names=F,
+                            stringsAsFactors=F)
 
 total_summary <- Suspension_Coil %>%
                  summarize(Mean=mean(PSI),
@@ -41,5 +41,22 @@ lot_summary <- Suspension_Coil %>%
                          Median=median(PSI),
                          Variance=var(PSI),
                          SD=sd(PSI))
-               
-               
+
+
+#############################
+# Deliverable 3
+#############################
+
+# compare total manufacturing sample versus population mean
+t.test(Suspension_Coil$PSI, mu=1500)
+
+# create subsets for each lot containing the PSI value
+lot1 <- subset(Suspension_Coil, Manufacturing_Lot == 'Lot1', select = c(PSI))
+lot2 <- subset(Suspension_Coil, Manufacturing_Lot == 'Lot2', select = c(PSI))
+lot3 <- subset(Suspension_Coil, Manufacturing_Lot == 'Lot3', select = c(PSI))
+
+# compare each sample (from the lots) to the population mean
+t.test(lot1, mu=1500)
+t.test(lot2, mu=1500)
+t.test(lot3, mu=1500)
+
